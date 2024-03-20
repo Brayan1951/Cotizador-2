749292097e92cfdba3e1af7ca8229c777c3733b8
Brayan1951 <brayanc1951@gmail.com>
2024-03-19 22:07:56 -0500

    fixed

--- src/Api/getCotizacion.js
+++ src/Api/getCotizacion.js
@@ -1,6 +1,5 @@
 import ExcelJS from "exceljs";
 import { saveAs } from 'file-saver';
-import { useCotizacionStore } from "../store/cotizacion";
 
 function cabecera(sheet){
 
@@ -211,60 +210,67 @@ sheet.getCell('L25').font =  {size: 11, color: { argb: 'FFFFFFFF' }, bold: true}
     
   }
   
-  function cierre(sheet) {
-    sheet.getCell(40,2).value='*El plazo máximo de entrega de los productos es de cinco (3) días hábiles contados a partir del día siguiente de la verificación del deposito en la cuenta de RASH PERU SAC'
-      sheet.mergeCells('B40:L40');
+  function cierre(sheet,row) {
+    console.log(row);
+    sheet.getCell(row,2).value='*El plazo máximo de entrega de los productos es de cinco (3) días hábiles contados a partir del día siguiente de la verificación del deposito en la cuenta de RASH PERU SAC'
+      sheet.mergeCells(`B${row}:L${row}`);
       
-      sheet.getCell(42,2).value='CUENTA CORRIENTE SOLES BCP'
-      sheet.mergeCells('B42:E42');
-      sheet.getCell('B42').alignment = { vertical: 'middle', horizontal: 'center' };
-      sheet.getCell('B42').fill = {      type: 'pattern',      pattern: 'solid',      fgColor: { argb: 'FFFF0000' }   }
-      sheet.getCell('B42').border = {      top: { style: 'thin' },      left: { style: 'thin' },      bottom: { style: 'thin' },      right: { style: 'thin' },    };
-      sheet.getCell('B42').font =  {size: 12, color: { argb: 'FFFFFFFF' }, bold: true}
-      sheet.getCell(43,2).value='SOLES:    193-1115038-0-09'
-      sheet.mergeCells('B43:E43');
-      sheet.getCell('B43').alignment = { vertical: 'middle', horizontal: 'center' };
-      sheet.getCell('B43').border = {      top: { style: 'thin' },      left: { style: 'thin' },      bottom: { style: 'thin' },      right: { style: 'thin' },    };
-      sheet.getCell(44,2).value='CCI:     002-193-001115038009-12'
-      sheet.mergeCells('B44:E44');
-      sheet.getCell('B44').alignment = { vertical: 'middle', horizontal: 'center' };
-      sheet.getCell('B44').border = {      top: { style: 'thin' },      left: { style: 'thin' },      bottom: { style: 'thin' },      right: { style: 'thin' },    };
+      sheet.getCell(row+2,2).value='CUENTA CORRIENTE SOLES BCP'
+      sheet.mergeCells(`B${row+2}:E${row+2}`);
+      sheet.getCell(`B${row+2}`).alignment = { vertical: 'middle', horizontal: 'center' };
+      sheet.getCell(`B${row+2}`).fill = {      type: 'pattern',      pattern: 'solid',      fgColor: { argb: 'FFFF0000' }   }
+      sheet.getCell(`B${row+2}`).border = {      top: { style: 'thin' },      left: { style: 'thin' },      bottom: { style: 'thin' },      right: { style: 'thin' },    };
+      sheet.getCell(`B${row+2}`).font =  {size: 12, color: { argb: 'FFFFFFFF' }, bold: true}
+      sheet.getCell(row+3,2).value='SOLES:    193-1115038-0-09'
+      sheet.mergeCells(`B${row+3}:E${row+3}`);
+      sheet.getCell(`B${row+3}`).alignment = { vertical: 'middle', horizontal: 'center' };
+      sheet.getCell(`B${row+3}`).border = {      top: { style: 'thin' },      left: { style: 'thin' },      bottom: { style: 'thin' },      right: { style: 'thin' },    };
+      sheet.getCell(row+4,2).value='CCI:     002-193-001115038009-12'
+      sheet.mergeCells(`B${row+4}:E${row+4}`);
+      sheet.getCell(`B${row+4}`).alignment = { vertical: 'middle', horizontal: 'center' };
+      sheet.getCell(`B${row+4}`).border = {      top: { style: 'thin' },      left: { style: 'thin' },      bottom: { style: 'thin' },      right: { style: 'thin' },    };
       
-      sheet.getCell(45,2).value='CUENTA CORRIENTE SOLES BBVA'
-      sheet.mergeCells('B45:E45');
-      sheet.getCell('B45').alignment = { vertical: 'middle', horizontal: 'center' };
-      sheet.getCell('B45').fill = {      type: 'pattern',      pattern: 'solid',      fgColor: { argb: 'FFFF0000' }   }
-      sheet.getCell('B45').border = {      top: { style: 'thin' },      left: { style: 'thin' },      bottom: { style: 'thin' },      right: { style: 'thin' },    };
-      sheet.getCell('B45').font =  {size: 12, color: { argb: 'FFFFFFFF' }, bold: true}
-      sheet.getCell(46,2).value='SOLES:   0011-0686-0100024765'
-      sheet.mergeCells('B46:E46');
-      sheet.getCell('B46').alignment = { vertical: 'middle', horizontal: 'center' };
-      sheet.getCell('B46').border = {      top: { style: 'thin' },      left: { style: 'thin' },      bottom: { style: 'thin' },      right: { style: 'thin' },    };
-      sheet.getCell(47,2).value='CCI:    011-686-000100024765-38'
-      sheet.mergeCells('B47:E47');
-      sheet.getCell('B47').alignment = { vertical: 'middle', horizontal: 'center' };
-      sheet.getCell('B47').border = {      top: { style: 'thin' },      left: { style: 'thin' },      bottom: { style: 'thin' },      right: { style: 'thin' },    };
-      
-      sheet.getCell(48,2).value='CUENTA CORRIENTE SOLES INTERBANK'
-      sheet.mergeCells('B48:E48');
-      sheet.getCell('B48').alignment = { vertical: 'middle', horizontal: 'center' };
-      sheet.getCell('B48').fill = {      type: 'pattern',      pattern: 'solid',      fgColor: { argb: 'FFFF0000' }   }
-      sheet.getCell('B48').border = {      top: { style: 'thin' },      left: { style: 'thin' },      bottom: { style: 'thin' },      right: { style: 'thin' },    };
-      sheet.getCell('B48').font =  {size: 12, color: { argb: 'FFFFFFFF' }, bold: true}
-      sheet.getCell(49,2).value='SOLES:   200-3000232023'
-      sheet.mergeCells('B49:E49');
-      sheet.getCell('B49').alignment = { vertical: 'middle', horizontal: 'center' };
-      sheet.getCell('B49').border = {      top: { style: 'thin' },      left: { style: 'thin' },      bottom: { style: 'thin' },      right: { style: 'thin' },    };
-      sheet.getCell(50,2).value='CCI:    003-200-003000232023-39'
-      sheet.mergeCells('B50:E50');
-      sheet.getCell('B50').alignment = { vertical: 'middle', horizontal: 'center' };
-      sheet.getCell('B50').border = {      top: { style: 'thin' },      left: { style: 'thin' },      bottom: { style: 'thin' },      right: { style: 'thin' },    };
+      sheet.getCell(row+5,2).value='CUENTA CORRIENTE SOLES BBVA'
+      sheet.mergeCells(`B${row+5}:E${row+5}`);
+      sheet.getCell(`B${row+5}`).alignment = { vertical: 'middle', horizontal: 'center' };
+      sheet.getCell(`B${row+5}`).fill = {      type: 'pattern',      pattern: 'solid',      fgColor: { argb: 'FFFF0000' }   }
+      sheet.getCell(`B${row+5}`).border = {      top: { style: 'thin' },      left: { style: 'thin' },      bottom: { style: 'thin' },      right: { style: 'thin' },    };
+      sheet.getCell(`B${row+5}`).font =  {size: 12, color: { argb: 'FFFFFFFF' }, bold: true}
       
       
+      sheet.getCell(row+6,2).value='SOLES:   0011-0686-0100024765'
+      sheet.mergeCells(`B${row+6}:E${row+6}`);
+      sheet.getCell(`B${row+6}`).alignment = { vertical: 'middle', horizontal: 'center' };
+      sheet.getCell(`B${row+6}`).border = {      top: { style: 'thin' },      left: { style: 'thin' },      bottom: { style: 'thin' },      right: { style: 'thin' },    };
       
-      sheet.getCell(42,8).value='Nota: Realizar los abonos solo a las cuentas de la empresa RASH PERU S.A.C.'
-      sheet.mergeCells('H42:L45');
-      sheet.getCell('H42').alignment = { vertical: 'top', wrapText: true }
+      
+      sheet.getCell(row+7,2).value='CCI:    011-686-000100024765-38'
+      sheet.mergeCells(`B${row+7}:E${row+7}`);
+      sheet.getCell(`B${row+7}`).alignment = { vertical: 'middle', horizontal: 'center' };
+      sheet.getCell(`B${row+7}`).border = {      top: { style: 'thin' },      left: { style: 'thin' },      bottom: { style: 'thin' },      right: { style: 'thin' },    };
+      
+      sheet.getCell(row+8,2).value='CUENTA CORRIENTE SOLES INTERBANK'
+      sheet.mergeCells(`B${row+8}:E${row+8}`);
+      sheet.getCell(`B${row+8}`).alignment = { vertical: 'middle', horizontal: 'center' };
+      sheet.getCell(`B${row+8}`).fill = {      type: 'pattern',      pattern: 'solid',      fgColor: { argb: 'FFFF0000' }   }
+      sheet.getCell(`B${row+8}`).border = {      top: { style: 'thin' },      left: { style: 'thin' },      bottom: { style: 'thin' },      right: { style: 'thin' },    };
+      sheet.getCell(`B${row+8}`).font =  {size: 12, color: { argb: 'FFFFFFFF' }, bold: true}
+     
+      sheet.getCell(row+9,2).value='SOLES:   200-3000232023'
+      sheet.mergeCells(`B${row+9}:E${row+9}`);
+      sheet.getCell(`B${row+9}`).alignment = { vertical: 'middle', horizontal: 'center' };
+      sheet.getCell(`B${row+9}`).border = {      top: { style: 'thin' },      left: { style: 'thin' },      bottom: { style: 'thin' },      right: { style: 'thin' },    };
+      
+      sheet.getCell(row+10,2).value='CCI:    003-200-003000232023-39'
+      sheet.mergeCells(`B${row+10}:E${row+10}`);
+      sheet.getCell(`B${row+10}`).alignment = { vertical: 'middle', horizontal: 'center' };
+      sheet.getCell(`B${row+10}`).border = {      top: { style: 'thin' },      left: { style: 'thin' },      bottom: { style: 'thin' },      right: { style: 'thin' },    };
+      
+      
+      
+      sheet.getCell(row+2,8).value='Nota: Realizar los abonos solo a las cuentas de la empresa RASH PERU S.A.C.'
+      sheet.mergeCells(`H${row+2}:L${row+5}`);
+      sheet.getCell(`H${row+2}`).alignment = { vertical: 'top', wrapText: true }
     
   }
   
@@ -278,7 +284,14 @@ sheet.getCell('L25').font =  {size: 11, color: { argb: 'FFFFFFFF' }, bold: true}
       const workbook = new ExcelJS.Workbook();
       const sheet = workbook.addWorksheet('Sheet1',{views: [{showGridLines: false}]});
       
-      
+      cabecera(sheet)
+      if (productos.length>7) {
+        cierre(sheet,31+productos.length)
+        
+      } else {
+        cierre(sheet,38)
+        
+      }
     
 
       const startRow=26
@@ -308,7 +321,7 @@ sheet.getCell('L25').font =  {size: 11, color: { argb: 'FFFFFFFF' }, bold: true}
       sheet.getCell(`J${refRow}`).border = {      top: { style: 'thin' },      left: { style: 'thin' },      bottom: { style: 'thin' },      right: { style: 'thin' },    };
       sheet.getCell(`K${refRow}`).border = {      top: { style: 'thin' },      left: { style: 'thin' },      bottom: { style: 'thin' },      right: { style: 'thin' },    };
       sheet.getCell(`L${refRow}`).border = {      top: { style: 'thin' },      left: { style: 'thin' },      bottom: { style: 'thin' },      right: { style: 'thin' },    };
-
+      // sheet.insertRows(refRow+1)
 
 
 
@@ -317,8 +330,7 @@ sheet.getCell('L25').font =  {size: 11, color: { argb: 'FFFFFFFF' }, bold: true}
       })
       
       
-      cabecera(sheet)
-      cierre(sheet)
+     
       // console.log(listProducts);
       
       // Comenzar a insertar desde la celda B26
